65adb5d84a3a612f151aeb9fe48af579fe74e77e
Kaif <kaif.enquiries@gmail.com>
2025-10-29 13:39:17 +0000

    Igneous Engine v2-rc1

--- Packages/Motus.lua
+++ Packages/Motus.lua
@@ -1,3 +1,3 @@
-local REQUIRED_MODULE = require(script.Parent._Index["notkaif_motus@1.0.5"]["motus"])
+local REQUIRED_MODULE = require(script.Parent._Index["notkaif_motus@1.1.0"]["motus"])
 export type MotusAnimator = REQUIRED_MODULE.MotusAnimator 
 return REQUIRED_MODULE
