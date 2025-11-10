199048aba81a65d2ba9b241e1f3dc413ca24045f
Kaif <kaif.enquiries@gmail.com>
2025-11-10 00:58:41 +0000

    Built and published by Moonwave

---
+++ assets/js/25fd074c.642f707d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[830],{4410:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/RedcliffStudios/Igneous/blob/master","baseUrl":"/Igneous/","classOrder":[{"section":"Classes","classes":["Igneous","IgneousWeapon","Modpack"],"collapsed":false}],"apiCategories":[]}')}}]);
